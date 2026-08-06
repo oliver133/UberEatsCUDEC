1faa8e09ef609bebb11f8376cf52a5186a099b45
oliver133 <olialejandro450@gmail.com>
2026-08-05 20:53:56 -0600

    yes

--- sw.js
+++ sw.js
@@ -1,7 +1,43 @@
-self.addEventListener('install', function(event){
-    event.waitUntill(
-        caches.open('sw- cache').then(function(cache){
-            return cache.add('index.html');
+const CACHE_NAME = 'parrivo-market-v1';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './css/styles.css',
+  './css/materialize.min.css'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(key) {
+          return key !== CACHE_NAME;
+        }).map(function(key) {
+          return caches.delete(key);
         })
-    );
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
 });
